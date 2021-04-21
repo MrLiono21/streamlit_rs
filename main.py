f396302b8291c5582adf82ecac61dd30da880569
MrLiono21 <lionelosamba@gmail.com>
2021-04-21 13:56:43 +0300

    employee_indices variable

--- main.py
+++ main.py
@@ -80,8 +80,8 @@ with model_training:
         sim_scores = list(enumerate(cosine_sim[idx]))
         sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
         sim_scores = sim_scores[1:11]
-        movie_indices = [i[0] for i in sim_scores]
-        return metadata['Employee_Name'].iloc[movie_indices]
+        employee_indices = [i[0] for i in sim_scores]
+        return metadata['Employee_Name'].iloc[employee_indices]
 
     recommender = get_recommendations(input_name)
 
